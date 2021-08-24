514e3e8d8939d7351a26181e7bba0d188cdd9401
brunosimm <bruno.simm@edu.pucrs.br>
2021-08-24 16:31:41 -0300

    Initial commit

---
+++ workspace/algafood-api/src/test/java/com/bsimm/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bsimm.algafood;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
